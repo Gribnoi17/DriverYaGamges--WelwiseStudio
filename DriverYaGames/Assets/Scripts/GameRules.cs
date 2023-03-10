66e8107430eb784b32eb6ca2e5a4ce68d1f3cbc1
Savely <sava_teplov@mail.ru>
2023-03-10 11:31:53 +0000

    Сделал основыные функции и примерный вид главного меню

--- DriverYaGames/Assets/Scripts/GameRules.cs
+++ DriverYaGames/Assets/Scripts/GameRules.cs
@@ -13,13 +13,35 @@ public class GameRules : MonoBehaviour
     [SerializeField] private int rateOfSpeedGrowth = 1;
     public int RateOfSpeedGrowth { get { return rateOfSpeedGrowth; } private set { rateOfSpeedGrowth = value; } }
 
+    [SerializeField] private int _startCarSpawnPeriod = 2;
+    public int StartCarSpawnPeriod { get { return _startCarSpawnPeriod; } private set { _startCarSpawnPeriod = value; } }
+
+    [SerializeField] private int _minCarSpawnPeriod = 1;
+    public int MinCarSpawnPeriod { get { return _minCarSpawnPeriod; } private set { _minCarSpawnPeriod = value; } }
+
+
+    private CarGenerator carGenerator;
 
     private void Awake()
     {
         StartSpeed = startSpeed;
         MaxSpeed = maxSpeed;
         RateOfSpeedGrowth = rateOfSpeedGrowth;
+        carGenerator = FindObjectOfType<CarGenerator>();
     }
 
+
+    private void Inizializition()
+    {
+        carGenerator.SpawnPeriod = _startCarSpawnPeriod;
+    }
+
+    public void SetCarSpawnPeriod(float val)
+    {
+        if (carGenerator.SpawnPeriod > _minCarSpawnPeriod)
+            carGenerator.SpawnPeriod -= val;
+        else
+            carGenerator.SpawnPeriod = _minCarSpawnPeriod;
+    }
 
 }
