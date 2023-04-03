0b8c70e376c973a64cd8356d0fd20a093273ecc9
VaVner <sergeantvavner@gmail.com>
2023-04-03 16:06:05 +0600

    начал делать ачивки

---
+++ DriverYaGames/Assets/Scripts/Achivments.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+public class Achivments : MonoBehaviour
+{
+    private int _allAchivments;
+    private List<int> _unfulfilledAchievements = new List<int>();
+
+
+    void AchivmentOne()
+    {
+
+    }
+
+    void AchivmentTwo()
+    {
+
+    }
+
+    void AchivmentThree()
+    {
+
+    }
+
+    void AchivmentFour()
+    {
+
+    }
+
+    void AchivmentFive()
+    {
+
+    }
+
+    void AchivmentSix()
+    {
+
+    }
+
+    void AchivmentSeven()
+    {
+
+    }
+
+    void AchivmentEight()
+    {
+
+    }
+
+    void AchivmentNine()
+    {
+
+    }
+
+    void AchivmentTen()
+    {
+
+    }
+}
