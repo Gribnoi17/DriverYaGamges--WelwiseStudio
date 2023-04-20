c1c17f5eecd4016300f76c8dcc3cc50460361025
VaVner <sergeantvavner@gmail.com>
2023-04-20 20:23:32 +0600

    Переделал активацию в гараже, подкоректировал аселератор

--- DriverYaGames/Assets/Scripts/AcsController.cs
+++ DriverYaGames/Assets/Scripts/AcsController.cs
@@ -7,11 +7,12 @@ using System;
 
 public class AcsController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rg;
+    [SerializeField] private Rigidbody[] _rg;
     [SerializeField] private float speed;
     [SerializeField] private float deadZone;
     [SerializeField] private float rotateSpeed;
     [SerializeField] private TextMeshProUGUI _value;
+    [SerializeField] private TextMeshProUGUI _valueForUnity;
 
     [Header("Coordinates")]
     [SerializeField] private float maxX;
@@ -19,40 +20,58 @@ public class AcsController : MonoBehaviour
 
     private Animator animator;
     private float _acceleration = 0;
+    [Header("For watch")]
+    [SerializeField]private Rigidbody _rB;
 
     public void GetNumber(float accelerationInHtml)
     {
-        _acceleration = accelerationInHtml;
+        _acceleration = accelerationInHtml / 10;
     }
 
     private void Start()
     {
-        animator = rg.GetComponent<Animator>();
+        foreach(Rigidbody r in _rg)
+        {
+            if (r.gameObject.activeSelf)
+            {
+                _rB = r;
+            }
+        }
+        animator = _rB.GetComponent<Animator>();
     }
 
     void Update()
     {
+      /*  Vector3 vec = Input.acceleration;
         try
         {
-            _value.text = Math.Truncate(_acceleration).ToString(); ;
+            _valueForUnity.text = vec.x.ToString();
+        }
+        catch
+        {
+            print("acs error");
+        }*/
+        try
+        {
+            _value.text = _acceleration.ToString();
         }
         catch
         {
             print("acs error");
         }
-        if (_acceleration > deadZone && rg.transform.position.x < maxX)
+        if (_acceleration > deadZone && _rB.transform.position.x < maxX)
         {
             var dir = new Vector3(_acceleration * speed, 0f, 0f);
-            rg.velocity = dir; 
+            _rB.velocity = dir; 
             animator.SetBool("None", false);
             animator.SetBool("RightTurn", true);
             animator.SetBool("LeftTurn", false);
 
         }
-        else if (_acceleration < -deadZone && rg.transform.position.x > minX)
+        else if (_acceleration < -deadZone && _rB.transform.position.x > minX)
         {
             var dir = new Vector3(_acceleration * speed, 0f, 0f);
-            rg.velocity = dir;
+            _rB.velocity = dir;
             animator.SetBool("None", false);
             animator.SetBool("LeftTurn", true);
             animator.SetBool("RightTurn", false);
@@ -60,7 +79,7 @@ public class AcsController : MonoBehaviour
         }
         else
         {
-            rg.velocity = new Vector3(0f, 0f, 0f);
+            _rB.velocity = new Vector3(0f, 0f, 0f);
                 animator.SetBool("None", true);
                 animator.SetBool("LeftTurn", false);
                 animator.SetBool("RightTurn", false);
